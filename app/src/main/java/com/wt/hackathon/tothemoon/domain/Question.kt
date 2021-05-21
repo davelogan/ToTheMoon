0802372e2073b3a00929c17b1483f6a6139eb336
David Logan <dlogan398@gmail.com>
2021-05-21 14:21:43 -0600

    added domain model and place holder images

---
+++ app/src/main/java/com/wt/hackathon/tothemoon/domain/Question.kt
@@ -0,0 +1,7 @@
+package com.wt.hackathon.tothemoon.domain
+
+
+sealed class Question {
+    class YesNoQuestion(val id: Int, val question: String, val  correctAnswer: Boolean): Question()
+    class ImageSetQuestion(val id: Int, val question: String, val answer: Array<Answer>): Question()
+}
---
+++ app/src/main/java/com/wt/hackathon/tothemoon/domain/Quiz.kt
@@ -0,0 +1,51 @@
+package com.wt.hackathon.tothemoon.domain
+
+import com.wt.hackathon.tothemoon.R
+
+class Quiz {
+    companion object {
+
+        //Question 1
+        val q1 = Question.YesNoQuestion(1, "Request Permission to Reenter Society", true)
+
+        //Question 2
+        val q2 = Question.ImageSetQuestion(2, "Select all Irresponsibly Dressed Humans",
+            arrayOf(
+                Answer(1, R.drawable.q2_a1, true),
+                Answer(2, R.drawable.q2_a2, false),
+                Answer(3, R.drawable.q2_a3, true),
+                Answer(4, R.drawable.q2_a4, true),
+                Answer(5, R.drawable.q2_a5, true),
+                Answer(6, R.drawable.q2_a6, false),
+                Answer(7, R.drawable.q2_a7, true),
+                Answer(8, R.drawable.q2_a8, true),
+                Answer(8, R.drawable.q2_a9, true)))
+
+        //Question 3
+        val q3 = Question.ImageSetQuestion(3, "Select all Irresponsibly Spaced Humans",
+            arrayOf(
+                Answer(1, R.drawable.q3_a1, true),
+                Answer(2, R.drawable.q3_a2, false),
+                Answer(3, R.drawable.q3_a3, true),
+                Answer(4, R.drawable.q3_a4, true),
+                Answer(5, R.drawable.q3_a5, true),
+                Answer(6, R.drawable.q3_a6, false),
+                Answer(7, R.drawable.q3_a7, true),
+                Answer(8, R.drawable.q3_a8, true),
+                Answer(8, R.drawable.q3_a9, true)))
+
+        //Question 4
+        val q4 = Question.YesNoQuestion(4, "Thank you for improving GovLife's Irresponsible Human Discovery Algorithm.\u2028\u2028A few more questions...", true)
+
+
+        //Question 5
+        val q5 = Question.YesNoQuestion(4, "Are you vaccinated?\u2028Yes / No\u2028", true)
+
+        //Question 6
+        val q6 = Question.YesNoQuestion(4, "You are free to reenter society.\u2028\u2028Please wear pants.", true)
+
+
+        val allQuestions = arrayOf(q1, q2, q3, q4, q5, q6)
+
+    }
+}
